9ca38e99fa8705e9fd2de3b5d16f82345207be80
문익현 <97443699+monikhyun@users.noreply.github.com>
2026-01-03 12:42:44 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        double x,y;
+
+      x = sc.nextInt();
+      y = sc.nextInt();
+
+      System.out.println(x/y);
+	}
+}
