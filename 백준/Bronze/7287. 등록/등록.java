06c855daa5b00903baf1aa6abf638aafaa16835d
문익현 <97443699+monikhyun@users.noreply.github.com>
2026-01-11 11:08:17 +0900

    [Bronze V] Title: 등록, Time: 108 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.append("16");
+        bw.newLine();
+        bw.append("mih2001103");
+        bw.flush();
+        bw.close();
+    }
+}
